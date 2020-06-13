fe01bf1f274d4d51fb41cc63bd6d6b1a8d3cf432
Tanzim Mokammel <mtanzim@gmail.com>
2020-06-13 17:54:34 +0200

    cache

--- src/components/Link.js
+++ src/components/Link.js
@@ -1,10 +1,60 @@
 import React from "react";
+import { AUTH_TOKEN } from "../constants";
+import { timeDifferenceForDate } from "../utils";
+import { Mutation } from "react-apollo";
+import gql from "graphql-tag";
 
-const Link = ({ link }) => {
+const VOTE_MUTATION = gql`
+  mutation VoteMutation($linkId: ID!) {
+    vote(linkId: $linkId) {
+      id
+      link {
+        id
+        votes {
+          id
+          user {
+            id
+          }
+        }
+      }
+      user {
+        id
+      }
+    }
+  }
+`;
+
+const Link = ({ link, index, updateStoreAfterVote }) => {
+  const authToken = localStorage.getItem(AUTH_TOKEN);
   return (
-    <div>
-      <div>
-        {link.description} ({link.url})
+    <div className="flex mt2 items-start">
+      <div className="flex items-center">
+        <span className="gray">{index + 1}.</span>
+        {authToken && (
+          <Mutation
+            update={(store, { data: { vote } }) =>
+              updateStoreAfterVote(store, vote, link.id)
+            }
+            mutation={VOTE_MUTATION}
+            variables={{ linkId: link.id }}
+          >
+            {(voteMutation) => (
+              <div className="ml1 gray f11" onClick={voteMutation}>
+                ▲
+              </div>
+            )}
+          </Mutation>
+        )}
+      </div>
+      <div className="ml1">
+        <div>
+          {link.description} ({link.url})
+        </div>
+        <div className="f6 lh-copy gray">
+          {link.votes.length} votes | by{" "}
+          {link.postedBy ? link.postedBy.name : "Unknown"}{" "}
+          {timeDifferenceForDate(link.createdAt)}
+        </div>
       </div>
     </div>
   );
--- src/components/LinkList.js
+++ src/components/LinkList.js
@@ -5,7 +5,7 @@ import gql from "graphql-tag";
 
 import { useQuery } from "@apollo/react-hooks";
 
-const FEED_QUERY = gql`
+export const FEED_QUERY = gql`
   {
     feed {
       links {
@@ -13,10 +13,29 @@ const FEED_QUERY = gql`
         createdAt
         url
         description
+        postedBy {
+          id
+          name
+        }
+        votes {
+          id
+          user {
+            id
+          }
+        }
       }
     }
   }
 `;
+
+const _updateCacheAfterVote = (store, createVote, linkId) => {
+  const data = store.readQuery({ query: FEED_QUERY });
+
+  const votedLink = data.feed.links.find((link) => link.id === linkId);
+  votedLink.votes = createVote.link.votes;
+
+  store.writeQuery({ query: FEED_QUERY, data });
+};
 
 const LinkList = () => {
   const { loading, error, data } = useQuery(FEED_QUERY);
@@ -29,8 +48,13 @@ const LinkList = () => {
   const linksToRender = data.feed.links;
   return (
     <div>
-      {linksToRender.map((link) => (
-        <Link key={link.id} link={link} />
+      {linksToRender.map((link, idx) => (
+        <Link
+          key={link.id}
+          link={link}
+          index={idx}
+          updateStoreAfterVote={_updateCacheAfterVote}
+        />
       ))}
     </div>
   );
